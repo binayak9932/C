12e03ef87bd31d6bbe352089bee270cd760131e7
Binayak Mondal <76868488+binayak9932@users.noreply.github.com>
2024-03-14 10:49:14 +0530

    Update

---
+++ jacobi.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_VARS 10
+#define MAX_ITER 1000
+
+void gauss_jacobi(double A[MAX_VARS][MAX_VARS], double B[MAX_VARS], double X[MAX_VARS], int n, double tol) {
+    int i, j, k;
+    double newX[MAX_VARS], error;
+
+    for (k = 0; k < MAX_ITER; k++) {
+        error = 0.0;
+        for (i = 0; i < n; i++) {
+            newX[i] = B[i];
+            for (j = 0; j < n; j++) {
+                if (j != i) {
+                    newX[i] -= A[i][j] * X[j];
+                }
+            }
+            newX[i] /= A[i][i];
+            error += fabs(newX[i] - X[i]);
+        }
+        for (i = 0; i < n; i++) {
+            X[i] = newX[i];
+        }
+        if (error < tol) {
+            printf("Converged after %d iterations\n", k + 1);
+            break;
+        }
+    }
+
+    if (k == MAX_ITER) {
+        printf("Did not converge after %d iterations\n", MAX_ITER);
+    }
+}
+
+int main() {
+    int n, i, j;
+    double A[MAX_VARS][MAX_VARS], B[MAX_VARS], X[MAX_VARS], tol;
+
+    printf("Enter the number of variables (maximum %d): ", MAX_VARS);
+    scanf("%d", &n);
+
+    printf("Enter the coefficients of the equations:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%lf", &A[i][j]);
+        }
+    }
+
+    printf("Enter the constants on the right-hand side:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%lf", &B[i]);
+    }
+
+    printf("Enter the tolerance limit: ");
+    scanf("%lf", &tol);
+
+    for (i = 0; i < n; i++) {
+        X[i] = 0.0;
+    }
+
+    gauss_jacobi(A, B, X, n, tol);
+
+    printf("Solution:\n");
+    for (i = 0; i < n; i++) {
+        printf("X[%d] = %lf\n", i, X[i]);
+    }
+
+    return 0;
+}
